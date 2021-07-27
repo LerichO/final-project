5d621d59a3708a572d306a9482917dfdfe87e581
Yasosalad <lerichyaso@gmail.com>
2021-07-27 19:01:32 +0000

    committing

--- model.py
+++ model.py
@@ -16,7 +16,9 @@ def search(term, location, api_key):
     response = requests.get(search_api_url, headers=headers, params=params, timeout=5).json()
     business_list = []
     for x in range(params["limit"]): 
-       business_list.append(response["businesses"][x])
+        business_list.append(response["businesses"][x])
+        # print(response["businesses"][x]["price"])
 
-    # -- ig monday we can start creating an algorithm that can somewhat identify small businesses --
+    # -- algorithm that can somewhat identify small businesses --
+    # -- might need to include Yelp Program API
     return business_list
